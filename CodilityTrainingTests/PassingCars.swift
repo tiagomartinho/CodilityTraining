0ee6290df97104a671d22d4b2b187baf9af1af9f
tiagomartinho <tiago.martinho@xpeppers.com>
2016-10-22 18:38:27 +0200

    One Pair Passing

--- CodilityTrainingTests/PassingCars.swift
+++ CodilityTrainingTests/PassingCars.swift
@@ -20,7 +20,29 @@ class PassingCars: XCTestCase {
         XCTAssertEqual(expected, output)
     }
 
+    func testOnePairPassing() {
+        let expected = 1
+        var input = [0, 1]
+
+        let output = solution(&input)
+
+        XCTAssertEqual(expected, output)
+    }
+
     public func solution(_ A : inout [Int]) -> Int {
-        return 0
+        let travelingEast = 0
+        let travelingWest = 1
+
+        var pairsOfPassingCars = 0
+        var isTravelingEast = false
+        for car in A {
+            if car == travelingEast {
+                isTravelingEast = true
+            }
+            if car == travelingWest && isTravelingEast {
+                pairsOfPassingCars += 1
+            }
+        }
+        return pairsOfPassingCars
     }
 }
