3ac113da20bf1762628961c0325024e9e9b14ad9
tiagomartinho <tiago.martinho@xpeppers.com>
2016-10-31 14:17:22 +0100

    adds CountDiv template

---
+++ CodilityTrainingTests/CountDiv.swift
@@ -0,0 +1,16 @@
+import XCTest
+
+class CountDiv: XCTestCase {
+
+    func testExample() {
+        let expected = 3
+
+        let output = solution(6, 11, 2)
+
+        XCTAssertEqual(expected, output)
+    }
+
+public func solution(_ A : Int, _ B : Int, _ K : Int) -> Int {
+        return 3
+    }
+}
