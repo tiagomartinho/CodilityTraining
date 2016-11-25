ca2571426f1a2823d09f1954149c3456396c052a
tiagomartinho <tiago.martinho@xpeppers.com>
2016-11-25 10:01:41 +0100

    extracts computePrefixes

--- CodilityTrainingTests/GenomicRangeQuery.swift
+++ CodilityTrainingTests/GenomicRangeQuery.swift
@@ -42,9 +42,29 @@ class GenomicRangeQuery: XCTestCase {
 
     public func solution(_ S : inout String, _ P : inout [Int], _ Q : inout [Int]) -> [Int] {
         let impactFactors: [Character: Int] = ["A": 1, "C": 2, "G": 3, "T": 4]
+        var prefixSum = computePrefixes(S)
+        var result = [Int](repeating: 0, count: P.count)
+        for (index,_) in P.enumerated() {
+            let x = P[index]
+            let y = Q[index]
+            for (a,_) in impactFactors.enumerated() {
+                var sub = 0
+                if x-1 >= 0 {
+                    sub = prefixSum[a][x-1]
+                }
+                if prefixSum[a][y] - sub > 0 {
+                    result[index] = a + 1
+                    break
+                }
+            }
+        }
+        return result
+    }
+
+    public func computePrefixes(_ S: String) -> [[Int]] {
+        let impactFactors: [Character: Int] = ["A": 1, "C": 2, "G": 3, "T": 4]
         let characters = [Int](repeating: 0, count: S.characters.count)
         var prefixSum = [[Int]](repeating: characters, count: impactFactors.count)
-
         for (index,nucleotide) in S.characters.enumerated() {
             if(nucleotide == "A") {
                 prefixSum[0][index] = 1
@@ -59,65 +79,11 @@ class GenomicRangeQuery: XCTestCase {
                 prefixSum[3][index] = 1
             }
         }
-
         for i in 1..<S.characters.count {
             for (j,_) in impactFactors.enumerated() {
                 prefixSum[j][i] += prefixSum[j][i-1]
             }
         }
-
-        var result = [Int](repeating: 0, count: P.count)
-
-        for (index,_) in P.enumerated() {
-            let x = P[index]
-            let y = Q[index]
-
-            for (a,_) in impactFactors.enumerated() {
-                var sub = 0
-                if x-1 >= 0 {
-                    sub = prefixSum[a][x-1]
-                }
-                if prefixSum[a][y] - sub > 0 {
-                    result[index] = a+1
-                    break
-                }
-            }
-        }
-
-        return result
+        return prefixSum
     }
-
-    public func calculatesMinimalImpact(query: Query, S: String) -> Int {
-        let minimumImpact = 1
-        let impactFactors: [Character: Int] = ["A": 1, "C": 2, "G": 3, "T": 4]
-        var minimalImpactForQuery = 4
-        for index in query.startIndex...query.endIndex {
-            if index >= S.characters.count { break }
-            let nucleotide = S[S.index(S.startIndex, offsetBy: index)]
-            if let impact = impactFactors[nucleotide] {
-                if impact == minimumImpact { return minimumImpact }
-                if impact < minimalImpactForQuery {
-                    minimalImpactForQuery = impact
-                }
-            }
-        }
-        return minimalImpactForQuery
-    }
-}
-
-public struct Query {
-    let startIndex: Int
-    let endIndex: Int
-}
-
-extension Query: Hashable {
-    public var hashValue: Int {
-        return startIndex.hashValue + endIndex.hashValue
-    }
-}
-
-extension Query: Equatable { }
-
-public func ==(lhs: Query, rhs: Query) -> Bool {
-    return lhs.startIndex == rhs.startIndex && lhs.startIndex == rhs.startIndex
 }
