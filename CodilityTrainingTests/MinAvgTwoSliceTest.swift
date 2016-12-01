af28a5b7670f160246bc583ee2f8d69118e7368f
Tiago Martinho <tiagomartinho@tiagos-mbp.lan>
2016-12-01 11:16:03 +0100

    solution for MinAvgTwoSlice

--- CodilityTrainingTests/MinAvgTwoSliceTest.swift
+++ CodilityTrainingTests/MinAvgTwoSliceTest.swift
@@ -12,6 +12,29 @@ class MinAvgTwoSliceTest: XCTestCase {
     }
     
     public func solution(_ A : inout [Int]) -> Int {
-        return 0
+        var minimumAverage = Double(A[0] + A[1]) / 2.0
+        var minimumPosition = 0
+        
+        for index in 0..<A.count-2 {
+            let twoElementAverage = Double(A[index] + A[index + 1]) / 2.0
+            if twoElementAverage < minimumAverage {
+                minimumAverage = twoElementAverage
+                minimumPosition = index
+            }
+            
+            let threeElementAverage = Double(A[index] + A[index + 1] + A[index + 2]) / 3.0
+            if threeElementAverage < minimumAverage {
+                minimumAverage = threeElementAverage
+                minimumPosition = index
+            }
+        }
+        
+        let lastTwoElementAverage = Double(A[A.count - 2] + A[A.count - 1]) / 2.0
+        if lastTwoElementAverage < minimumAverage {
+            minimumAverage = lastTwoElementAverage
+            minimumPosition = A.count - 2
+        }
+        
+        return minimumPosition
     }
 }
